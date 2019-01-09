10e53574474f8f17b8acf9d21760341cb368086c
Tetheus Trab <matheusferreira_mfn@hotmail.com>
2019-01-09 12:00:42 -0200

    "Bagulho tá funcional, pingando direitinho e contando as vezes que deu ruim na pingada, além de ser possível alterar o IP que vai pingar"

--- src/view/TelaDaPingada.java
+++ src/view/TelaDaPingada.java
@@ -6,6 +6,7 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.border.EmptyBorder;
 import controller.TestarPingController;
+
 import javax.swing.JLabel;
 import java.awt.Font;
 import javax.swing.JTextArea;
@@ -23,6 +24,8 @@ public class TelaDaPingada extends JFrame {
 	private JLabel lblPseudocmd, lblQuedas;
 	
 	private TestarPingController testadorDePing;
+	private JTextField txtIp;
+	private JTextField txtTempo;
 
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
@@ -40,6 +43,8 @@ public class TelaDaPingada extends JFrame {
 	}
 
 	public TelaDaPingada() {
+		super("Um teste pro foxtrotos");
+		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 540, 320);
 		contentPane = new JPanel();
@@ -75,10 +80,34 @@ public class TelaDaPingada extends JFrame {
 		btnStart = new JButton("Iniciar");
 		btnStart.setFont(new Font("Verdana", Font.PLAIN, 13));
 		btnStart.setBounds(417, 196, 89, 32);
-		contentPane.add(btnStart);
+		contentPane.add(btnStart);		
 		
-		testadorDePing = new TestarPingController(txtAreaCmd, txtQuedas);
-		btnStart.addActionListener(testadorDePing);		
+		txtIp = new JTextField("8.8.8.8");
+		txtIp.setFont(new Font("Verdana", Font.PLAIN, 13));
+		txtIp.setColumns(10);
+		txtIp.setBounds(417, 96, 97, 26);
+		contentPane.add(txtIp);
 		
+		JLabel lblIp = new JLabel("IP para Teste");
+		lblIp.setFont(new Font("Verdana", Font.PLAIN, 13));
+		lblIp.setBounds(417, 72, 97, 14);
+		contentPane.add(lblIp);
+		
+		txtTempo = new JTextField();
+		txtTempo.setFont(new Font("Verdana", Font.PLAIN, 15));
+		txtTempo.setEditable(false);
+		txtTempo.setColumns(10);
+		txtTempo.setBounds(417, 159, 97, 26);
+		contentPane.add(txtTempo);
+		
+		JLabel lblTempoCorrido = new JLabel("Tempo Corrido");
+		lblTempoCorrido.setFont(new Font("Verdana", Font.PLAIN, 13));
+		lblTempoCorrido.setBounds(417, 135, 97, 14);
+		contentPane.add(lblTempoCorrido);
+		
+		testadorDePing = new TestarPingController(txtAreaCmd, txtQuedas, btnStart, txtIp);
+		
+		btnStart.addActionListener(testadorDePing);	      
+	        
 	}
 }
